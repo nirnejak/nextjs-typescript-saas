fb2911ae79d0dbd1fb381dcad42b6675ce3b96d2
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2026-01-14 15:47:40 +0530

    eslint config bugfix

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import { defineConfig, globalIgnores } from "eslint/config"
 import nextVitals from "eslint-config-next/core-web-vitals"
 import nextTs from "eslint-config-next/typescript"
 import prettierConfig from "eslint-config-prettier"
-
+import prettierPlugin from "eslint-plugin-prettier"
 import promise from "eslint-plugin-promise"
 import betterTailwindcss from "eslint-plugin-better-tailwindcss"
 
