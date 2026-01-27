164af2a1b63f2471e9dd87bbb7bd4472aa9c8a70
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2026-01-28 00:41:57 +0530

    Project Details: NextAuth → BetterAuth

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ const config = {
   baseUrl: "https://nextjs-typescript-saas.vercel.app",
   appName: "Next.js TypeScript SaaS Starter",
   appDescription:
-    "Next.js starter for building SaaS products configured TypeScript, Drizzle, NextAuth, ESLint, Tailwind, Framer Motion etc.",
+    "Next.js starter for building SaaS products configured TypeScript, Drizzle, BetterAuth, ESLint, Tailwind, Framer Motion etc.",
   creator: "<Creator Name>",
   authorName: "<Author Name>",
   authorUrl: "<Author Name or Email>",
